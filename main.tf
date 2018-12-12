345bdf46f1a4df5429bb7666d2d989f7ddf9c8b8
Miguel Bernadin <Miguel.Bernadin@gmail.com>
2018-12-12 15:18:10 -0800

    updating the version properly pin patch version

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@
  *```hcl
  * module "lb-public-agents" {
  *   source  = "dcos-terraform/lb-public-agents/azurerm"
- *   version = "~> 0.1"
+ *   version = "~> 0.1.0"
  *
  *   cluster_name = "production"
  *
